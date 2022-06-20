54f3b7162c9ccc069976c26e11486dde6691cec7
George Pisaltu <georgep@casperlabs.io>
2022-06-20 15:20:06 +0300

    Add `archive create` subcommand
    Signed-off-by: George Pisaltu <georgep@casperlabs.io>

--- src/subcommands/archive/tar_utils.rs
+++ src/subcommands/archive/tar_utils.rs
@@ -4,7 +4,21 @@ use std::{
     path::PathBuf,
 };
 
-use tar::Archive;
+use tar::{Archive, Builder};
+
+pub fn archive(dir: &PathBuf, tarball_path: &PathBuf) -> Result<(), IoError> {
+    let temp_tarball_file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(tarball_path)?;
+    let mut tarball_stream = Builder::new(temp_tarball_file);
+    for file in std::fs::read_dir(dir).unwrap() {
+        if let Ok(entry) = file {
+            tarball_stream.append_path(entry.path())?;
+        }
+    }
+    tarball_stream.finish()
+}
 
 pub fn unarchive(src: PathBuf, dest: PathBuf) -> Result<(), IoError> {
     let input = OpenOptions::new().read(true).open(src)?;
